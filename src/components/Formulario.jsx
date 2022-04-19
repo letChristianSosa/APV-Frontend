082e9a0415a511b03ca2035aca9283366466acac
Christian Sosa <chrissm28@outlook.com>
2022-04-18 22:50:08 -0500

    Added Formulario and ListadoPacientes components

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,15 @@
+const Formulario = () => {
+  return (
+     <>
+          <p className="text-lg text-center mb-10">Anade tus pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
+
+          <form>
+               <div className="mb-5">
+                    <label>Nombre Mascota</label>
+               </div>
+          </form>
+     </>
+  )
+}
+
+export default Formulario
