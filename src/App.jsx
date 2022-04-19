4627245ae5bf70e1b33bee5c0808251f7e6b3131
Christian Sosa <chrissm28@outlook.com>
2022-04-18 21:31:43 -0500

    Added authentication, Hooks and more

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import AuthLayout from "./layout/AuthLayout";
+import RutaProtegida from './layout/RutaProtegida';
+
 import Login from './pages/Login';
 import OlvidePassword from './pages/OlvidePassword';
 import Registrar from './pages/Registrar';
 import ConfirmarCuenta from './pages/ConfirmarCuenta';
+import NuevoPassword from './pages/NuevoPassword';
+import AdministrarPacientes from './pages/AdministrarPacientes';
+
+import { AuthProvider } from './context/AuthProvider'
 
 function App() {
   console.log(import.meta.env.VITE_BACKEND_URL);
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<AuthLayout/>}>
-          <Route index element={<Login/>}/>
-          <Route path='registrar' element={<Registrar/>}/>
-          <Route path='olvide-password' element={<OlvidePassword/>}/>
-          <Route path='confirmar/:id' element={<ConfirmarCuenta/>}/>
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+
+          {/* Area Publica */}
+          <Route path='/' element={<AuthLayout/>}>
+            <Route index element={<Login/>}/>
+            <Route path='registrar' element={<Registrar/>}/>
+            <Route path='olvide-password' element={<OlvidePassword/>}/>
+            <Route path='olvide-password/:token' element={<NuevoPassword/>}/>
+            <Route path='confirmar/:id' element={<ConfirmarCuenta/>}/>
+          </Route>
+
+          {/* Area Privada */}
+          <Route path='/admin' element={<RutaProtegida/>}>
+            <Route index element={<AdministrarPacientes/>}/>
+          </Route>
+
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 }
