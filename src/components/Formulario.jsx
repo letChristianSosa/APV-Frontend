84d0aca4b1f961bd3ca689320c6940121a695529
Christian Sosa <chrissm28@outlook.com>
2022-04-23 15:02:42 -0500

    Avance en el componente Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,36 +1,59 @@
+import {useState} from 'react';
+import Alerta from './Alerta';
+import usePacientes from '../hooks/usePacientes';
+
 const Formulario = () => {
+     const [nombre, setNombre] = useState('');
+     const [propietario, setPropietario] = useState('');
+     const [email, setEmail] = useState('');
+     const [fecha, setFecha] = useState(Date.now());
+     const [sintomas, setSintomas] = useState('');
+
+     const [alerta, setAlerta] = useState({});
+
+     const {pacientes} = usePacientes();
+
+     const handleSubmit = e => {
+          e.preventDefault();
+          if([nombre, propietario, email, fecha, sintomas].includes('')){
+               setAlerta({error: true, msg: 'Todos los campos son obligatorios'});
+               return;
+          }
+     }
+
   return (
      <>
           <p className="text-lg text-center mb-10">Anade tus pacientes y <span className="text-indigo-600 font-bold">Administralos</span></p>
 
-          <form className="bg-white py-10 px-5 mb-10 lg:mb-0 shadow-md rounded-md">
+          <form className="bg-white py-10 px-5 mb-10 lg:mb-5 shadow-md rounded-md" onSubmit={handleSubmit}>
                <div className="mb-5">
-                    <label htmlFor="mascota" className="text-gray-700 uppercase font-bold">Nombre Mascota</label>
-                    <input id= "mascota" type="text" placeholder="Nombre de la Mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                    <label htmlFor="nombre" className="text-gray-700 uppercase font-bold">Nombre Mascota</label>
+                    <input id= "nombre" type="text" placeholder="Nombre de la Mascota" value={nombre} onChange={e => setNombre(e.target.value)} className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
                </div>
 
                <div className="mb-5">
                     <label htmlFor="propietario" className="text-gray-700 uppercase font-bold">Nombre Propietario</label>
-                    <input id= "propietario" type="text" placeholder="Nombre del Propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                    <input id= "propietario" type="text" placeholder="Nombre del Propietario" value={propietario} onChange={e => setPropietario(e.target.value)} className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
                </div>
 
                <div className="mb-5">
                     <label htmlFor="email" className="text-gray-700 uppercase font-bold">Email Propietario</label>
-                    <input id= "email" type="email" placeholder="Email del Propietario" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                    <input id= "email" type="email" placeholder="Email del Propietario" value={email} onChange={e => setEmail(e.target.value)} className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
                </div>
 
                <div className="mb-5">
                     <label htmlFor="fecha" className="text-gray-700 uppercase font-bold">Fecha Alta</label>
-                    <input id= "fecha" type="date" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                    <input id= "fecha" type="date" value={fecha} onChange={e => setFecha(e.target.value)} className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
                </div>
 
                <div className="mb-5">
                     <label htmlFor="sintomas" className="text-gray-700 uppercase font-bold">Sintomas</label>
-                    <textarea id= "sintomas" placeholder="Describe los Sintomas" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
+                    <textarea id= "sintomas" placeholder="Describe los Sintomas" value={sintomas} onChange={e => setSintomas(e.target.value)} className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"/>
                </div>
 
                <input type="submit" value="Agregar Paciente" className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"/>
           </form>
+          {alerta.msg && <Alerta alerta = {alerta}/>}
      </>
   )
 }
