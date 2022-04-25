3167f6da9effb7bd8c6951ccfd1494b83e6eefb8
Christian Sosa <chrissm28@outlook.com>
2022-04-25 03:29:14 -0500

    Added comment for visitants

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -47,9 +47,13 @@ const Login = () => {
       
         <div>
           <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión y Administra tus <span className="text-black">Pacientes</span></h1>
+          
         </div>          
         <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
           {msg && <Alerta alerta={alerta}/>}
+          <p className="block text-center my-5 text-gray-500">This app was made using mailtrap.</p>
+          <p className="block text-center my-5 text-gray-500">You can try it with:</p>
+          <p className="block text-center my-5 text-gray-500">Email: example@email.com and Password: 123456</p>
           <form onSubmit={handleSubmit}>
             <div className="my-5">
               <label className="uppercase text-gray-600 block text-xl font-bold">Email</label>
